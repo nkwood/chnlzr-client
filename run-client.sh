40550a1b79072be2bc448bf32a302df919dfdc31
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-02-20 11:47:24 -0600

    bump chnlzr-common dep

--- run-client.sh
+++ run-client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "target/chnlzr-client-0.3.3.jar" -Dorg.slf4j.simpleLogger.defaultLogLevel=debug org.anhonesteffort.chnlzr.ChnlzrClient "$@"
+java -cp "target/chnlzr-client-0.3.4.jar" -Dorg.slf4j.simpleLogger.defaultLogLevel=debug org.anhonesteffort.chnlzr.ChnlzrClient "$@"
